430eb0b8ac7044d54c65b0d257dcf872438d019e
Haroun Mamalo <mamalostar12311@gmail.com>
2025-08-23 18:31:09 +0800

    first commit

---
+++ src/components/JobListings.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { ref, reactive, defineProps, onMounted } from "vue";
+import { RouterLink } from "vue-router";
+// import jobData from '@/jobs2.json';
+import JobListing from "./JobListing.vue";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
+import axios from "axios";
+
+const state = reactive({
+  jobs: [],
+  isLoading: true,
+});
+
+onMounted(async () => {
+  try {
+    const response = await axios.get("/api/jobs");
+    state.jobs = response.data;
+  } catch (error) {
+    console.error("Error fetching jobs", error);
+  } finally {
+    state.isLoading = false;
+  }
+});
+
+defineProps({
+  limit: Number,
+  showButton: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <section class="bg-blue-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+      <!-- Show loading spinner while loading is true -->
+      <div class="text-center text-gray-500" v-if="state.isLoading">
+        <PulseLoader />
+      </div>
+
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6" v-else>
+        <JobListing
+          v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
+          :key="job.id"
+          :job="job"
+        />
+      </div>
+    </div>
+  </section>
+
+  <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+    <RouterLink
+      to="/jobs"
+      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >View All Jobs</RouterLink
+    >
+  </section>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
+
+</script>
+
+<template>
+  <Hero 
+      title="Find A Job" 
+      subtitle="With our innovative and advanced webscrapping technology, 
+      we're able to catch all the entire available jobs in most of well-known companies 
+      and we help them apply direct to you" 
+  />
+  <HomeCards />
+  <JobListings
+    :limit="3"
+    :showButton="true"
+    />
+</template>
